a90f7378848b69b85e43eb25935a63e2f2b3367c
singsai <crazyfarm@Nicks-MacBook-Pro.local>
2012-06-14 16:01:42 -0600

    Edit application config

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,10 @@ module Pling
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
+    
+    #to get Heroku to work
+    config.assets.initialize_on_precompile = false
+    
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
