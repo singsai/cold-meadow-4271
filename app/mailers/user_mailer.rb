54509149c526f52a62e2bddc8260480895d8c716
singsai <crazyfarm@Nicks-MacBook-Pro.local>
2012-06-15 00:52:24 -0600

    test change of bcc

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,7 +16,7 @@ class UserMailer < ActionMailer::Base
       <a href='http://www.facebook.com/pages/PalmLing/108198825934073'>
       <img src='http://therohanaurora.com/files/downloads/icon/facebook_16.png' /></a><span> </span><a href='http://www.facebook.com/pages/PalmLing/108198825934073'>PalmLing on FaceBook</a>")
     else
-      mail(:to => user.email, :bcc => "info@palmling.com", :subject => "Thank you for Signing up with PalmLing for Skype!", 
+      mail(:to => user.email, :bcc => "nick@palmling.com", :subject => "Thank you for Signing up with PalmLing for Skype!", 
       :content_type => "text/html", :body => "Thank you for signing up with <a href='http://www.palmling.com'>PalmLing for Skype</a>. Your Skype name is: #{user.skype} <br/><br/>Once your payment is processed and confirmed, a member of the PalmLing team will follow-up with you with details on how to
       access our global community of translators. Please allow 1 hour for us to respond to you. <br/><br/>Thank you, <br/><br/>
       The PalmLing Team, <br/><a href='http://www.palmling.com'>PalmLing<a/>. <br/><br/>
