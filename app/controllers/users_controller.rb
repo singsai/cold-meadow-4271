43c9710c9545db7e45f6074a8d020c5c463215a7
singsai <crazyfarm@Nicks-MacBook-Pro.local>
2012-06-14 20:40:00 -0600

    Edit route, add thankyou pages, change logic in users controller to allow for a palmling free redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,20 +8,37 @@ class UsersController < ApplicationController
     @user.user_type = params[:user_type]
     #UserMailer.test_email(self).deliver
   end
-  
+    
   def create
-      @user = User.new(params[:user])
-      if @user.save
-        flash[:success] = "Thanks for signing up"
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Thanks for signing up"
         
-        #SEND EMAIL THANKYOU
+      #SEND EMAIL THANKYOU
         
-        redirect_to @user
-      else
-        render 'new'
-      end            
-    end
+      #redirect_to 'static_pages#thanks'
+      #render :action => 'thanks_free'
 
+      #redirect_to 'static_pages#thanks_free'
+        
+      if request.referer.include? "free"
+        render 'thanks_free'  
+      else  
+        redirect_to @user
+      end
+      
+    else
+      render 'new'
+    end            
+  end
+
+  def new_free
+    @user = User.new
+    @user.user_type = params[:user_type]
+  end
   
+  def thanks_free
+    redirect_to 'http://www.google.com'
+  end
 
 end
