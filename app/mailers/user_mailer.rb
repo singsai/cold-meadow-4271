26d29d4c8f2d3c2e09a66da6fb7c3d076027598a
singsai <crazyfarm@Nicks-MacBook-Pro.local>
2012-06-15 14:25:23 -0600

    Minor edits to language

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user, version)
     if version == "free"
       mail(:to => user.email, :bcc => "info@palmling.com", :subject => "Thank you for Signing up with PalmLing for Skype!", 
-      :content_type => "text/html", :body => "Thank you for signing up for a trial version of <a href='http://www.palmling.com'>PalmLing for Skype</a>. Your Skype name is: #{user.skype} 
+      :content_type => "text/html", :body => "Thank you for signing up for a trial version of <a href='http://skype.palmling.com'>PalmLing for Skype</a>. Your Skype name is: #{user.skype} 
       <br/><br/>
       Your trial version will expire in one hour. To get started, simply send a Skype request to <i>PalmLingSpanish</i>.<br/><br/>
       Once we confirm your request, you will have access for one hour to
@@ -17,7 +17,7 @@ class UserMailer < ActionMailer::Base
       <img src='http://therohanaurora.com/files/downloads/icon/facebook_16.png' /></a><span> </span><a href='http://www.facebook.com/pages/PalmLing/108198825934073'>FaceBook</a>")
     else
       mail(:to => user.email, :bcc => "info@palmling.com", :subject => "Thank you for Signing up with PalmLing for Skype!", 
-      :content_type => "text/html", :body => "Thank you for signing up with <a href='http://www.palmling.com'>PalmLing for Skype</a>. Your Skype name is: #{user.skype} <br/><br/>Once your payment is processed and confirmed, a member of the PalmLing team will follow-up with you with details on how to
+      :content_type => "text/html", :body => "Thank you for signing up with <a href='http://skype.palmling.com'>PalmLing for Skype</a>. Your Skype name is: #{user.skype} <br/><br/>Once your payment is processed and confirmed, a member of the PalmLing team will follow-up with you with details on how to
       access our global community of translators. Please allow 1 hour for us to respond to you. <br/><br/>Thank you, <br/><br/>
       The PalmLing Team, <br/><a href='http://www.palmling.com'>PalmLing<a/>. <br/><br/>
       <a href='https://twitter.com/#!/palmling'><img src='http://dl.dropbox.com/u/3722777/icon/twitter_16.png' /></a><span> </span><a href='https://twitter.com/#!/palmling'>Twitter</a><br/>
