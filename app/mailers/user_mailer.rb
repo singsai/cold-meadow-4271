dccc4b25872eb6860301d22e213e3b7f013da9a4
singsai <crazyfarm@Nicks-MacBook-Pro.local>
2012-06-15 01:10:32 -0600

    Add Your trial version will expire in one hour.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ActionMailer::Base
       mail(:to => user.email, :bcc => "info@palmling.com", :subject => "Thank you for Signing up with PalmLing for Skype!", 
       :content_type => "text/html", :body => "Thank you for signing up for a trial version of <a href='http://www.palmling.com'>PalmLing for Skype</a>. Your Skype name is: #{user.skype} 
       <br/><br/>
-      To get started, simply send a Skype request to <i>PalmLingSpanish</i>.<br/><br/>
+      Your trial version will expire in one hour. To get started, simply send a Skype request to <i>PalmLingSpanish</i>.<br/><br/>
       Once we confirm your request, you will have access for one hour to
       access our global community of translators. Please don't abuse this privilege, and don't worry, your trial version will not start until we confirm your request. Please allow 1 hour for us to respond to you. 
       If you would like to sign up for the premium version, please visit: <a href='http://skype.palmling.com'>skype.palmling.com</a>.
