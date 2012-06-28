669ca5a7fe262ab05e651ac466fd35388d912ed7
singsai <crazyfarm@Nicks-MacBook-Pro.local>
2012-06-28 06:08:23 -0600

    Fix mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,27 +1,27 @@
 class UserMailer < ActionMailer::Base
-  default :from => "info@palmling.com"
+  default :from => "info@verbalizeit.com"
   
   def welcome_email(user, version)
     if version == "free"
-      mail(:to => user.email, :bcc => "info@palmling.com", :subject => "Thank you for Signing up with PalmLing for Skype!", 
-      :content_type => "text/html", :body => "Thank you for signing up for a trial version of <a href='http://skype.palmling.com'>PalmLing for Skype</a>. Your Skype name is: #{user.skype} 
+      mail(:to => user.email, :bcc => "info@verbalizeit.com", :subject => "Thank you for Signing up with VerbalizeIt for Skype!", 
+      :content_type => "text/html", :body => "Thank you for signing up for a trial version of <a href='http://skype.verbalizeit.com'>VerbalizeIt for Skype</a>. Your Skype name is: #{user.skype} 
       <br/><br/>
-      You will soon receive a Skype request from 'PalmLingSpanish'.
+      You will soon receive a Skype request from 'VerbalizeItSpanish'.
       Once you confirm our request, you will be able to make 1 call to access  our global community of translators. 
-      You can make additional calls by signing up for the <a href='http://skype.palmling.com'>premium version</a> by visiting <a href='http://skype.palmling.com'>skype.palmling.com</a>.
+      You can make additional calls by signing up for the <a href='http://skype.verbalizeit.com'>premium version</a> by visiting <a href='http://skype.verbalizeit.com'>skype.verbalizeit.com</a>.
       <br/><br/>Thank you, <br/><br/>
-      The PalmLing Team, <br/><a href='http://www.palmling.com'>PalmLing<a/>. <br/><br/>
-      <a href='https://twitter.com/#!/palmling'><img src='http://dl.dropbox.com/u/3722777/icon/twitter_16.png' /></a><span> </span><a href='https://twitter.com/#!/palmling'>Twitter</a><br/>
-      <a href='http://www.facebook.com/pages/PalmLing/108198825934073'>
-      <img src='http://therohanaurora.com/files/downloads/icon/facebook_16.png' /></a><span> </span><a href='http://www.facebook.com/pages/PalmLing/108198825934073'>FaceBook</a>")
+      The VerbalizeIt Team, <br/><a href='http://www.verbalizeit.com'>VerbalizeIt<a/>. <br/><br/>
+      <a href='https://twitter.com/#!/verbalize_it'><img src='http://dl.dropbox.com/u/3722777/icon/twitter_16.png' /></a><span> </span><a href='https://twitter.com/#!/verbalize_it'>Twitter</a><br/>
+      <a href='http://www.facebook.com/verbalizeit'>
+      <img src='http://therohanaurora.com/files/downloads/icon/facebook_16.png' /></a><span> </span><a href='http://www.facebook.com/verbalizeit'>FaceBook</a>")
     else
-      mail(:to => user.email, :bcc => "info@palmling.com", :subject => "Thank you for Signing up with PalmLing for Skype!", 
-      :content_type => "text/html", :body => "Thank you for signing up with <a href='http://skype.palmling.com'>PalmLing for Skype</a>. Your Skype name is: #{user.skype} <br/><br/>Once your payment is processed and confirmed, a member of the PalmLing team will follow-up with you with details on how to
+      mail(:to => user.email, :bcc => "info@verbalizeit.com", :subject => "Thank you for Signing up with VerbalizeIt for Skype!", 
+      :content_type => "text/html", :body => "Thank you for signing up with <a href='http://skype.verbalizeit.com'>VerbalizeIt for Skype</a>. Your Skype name is: #{user.skype} <br/><br/>Once your payment is processed and confirmed, a member of the VerbalizeIt team will follow-up with you with details on how to
       access our global community of translators. Please allow 1 hour for us to respond to you. <br/><br/>Thank you, <br/><br/>
-      The PalmLing Team, <br/><a href='http://www.palmling.com'>PalmLing<a/>. <br/><br/>
-      <a href='https://twitter.com/#!/palmling'><img src='http://dl.dropbox.com/u/3722777/icon/twitter_16.png' /></a><span> </span><a href='https://twitter.com/#!/palmling'>Twitter</a><br/>
-      <a href='http://www.facebook.com/pages/PalmLing/108198825934073'>
-      <img src='http://therohanaurora.com/files/downloads/icon/facebook_16.png' /></a><span> </span><a href='http://www.facebook.com/pages/PalmLing/108198825934073'>Facebook</a>")
+      The VerbalizeIt Team, <br/><a href='http://www.verbalizeit.com'>VerbalizeIt<a/>. <br/><br/>
+      <a href='https://twitter.com/#!/verbalize_it'><img src='http://dl.dropbox.com/u/3722777/icon/twitter_16.png' /></a><span> </span><a href='https://twitter.com/#!/verbalize_it'>Twitter</a><br/>
+      <a href='http://www.facebook.com/verbalizeit'>
+      <img src='http://therohanaurora.com/files/downloads/icon/facebook_16.png' /></a><span> </span><a href='http://www.facebook.com/verbalizeit'>Facebook</a>")
     end
   end
 
